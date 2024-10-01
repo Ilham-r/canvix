f2831f96ecb9a132c591905780f8797b38f72af5
Ilham-r <66206508+Ilham-r@users.noreply.github.com>
2024-10-01 13:47:52 +0100

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,27 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        LightGreen: "#64CCC5",
+        LightGray: "#F4F4F4",
+        LightBlack: "#0C0C0C",
+        DarkGray: "#DFDFDF",
+      },
+      fontSize: {
+        h1: ["80px"],
+        h2: ["77px"],
+        h3: ["64px"],
+        h4: ["45px"],
+        h5: ["30px"],
+        h6: ["20px"],
+        Bodytext: ["18px"],
+        bodysmall: ["16px"],
+      },
+      fontWeight: {
+        h1: "600", // Semi-bold
+        h2: "700",
+        h3: "600",
+        h4: "700",
+        h5: "600",
       },
     },
   },
