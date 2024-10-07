720f7a0c3abb84649922eb3cb9add7696f4a4be9
Ilham-r <66206508+Ilham-r@users.noreply.github.com>
2024-10-07 15:44:40 +0100

    homepage

--- app/page.js
+++ app/page.js
@@ -1,11 +1,8 @@
-import AboutUs from "./About/page";
 import HomePage from "./Home/page";
-import Portfolio from "./Portfolio/page";
-
 export default function Home() {
   return (
     <div>
-      <Home />
+      <HomePage />
     </div>
   );
 }
